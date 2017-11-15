0dc3c0fabf123031f147014ec060138e26d63c83
Linus Koepfer <linkoep@gmail.com>
2017-11-15 02:10:12 -0500

    First working code searches twitter, posts hello world

--- index.js
+++ index.js
@@ -3,4 +3,31 @@ console.log('Starting bot');
 var Twit = require('twit');
 var config = require('./config');
 
-var T = new Twit(config)
+var T = new Twit(config);
+
+//  search twitter for all tweets containing the word 'I'm'
+var search_params = { 
+	q: 'I\'m', 
+	count: 2 
+};
+
+T.get('search/tweets', search_params, gotData);
+
+function gotData(err, data, response) {
+ 	var tweets = data.statuses;
+ 	for (var i = 0; i < tweets.length; i++) {
+ 		console.log(tweets[i].text);
+ 	}
+}
+
+//  tweet 'hello world!'
+var post_params = { status: 'hello world!' };
+
+T.post('statuses/update', post_params, postedTweet);
+
+function postedTweet(err, data, response) {
+  if (err) {
+  	console.log(err);
+  }
+  console.log("Tweet posted");
+}
