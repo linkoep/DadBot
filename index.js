1b1ca01d7f4533c580b4c0d3a2b8bca605362489
Linus Koepfer <linkoep@gmail.com>
2017-11-15 03:30:49 -0500

    First working search and reply Todo: Make it actually reply, refine search, stream

--- index.js
+++ index.js
@@ -8,26 +8,45 @@ var T = new Twit(config);
 //  search twitter for all tweets containing the word 'I'm'
 var search_params = { 
 	q: 'I\'m', 
-	count: 2 
+	count: 10 
 };
 
 T.get('search/tweets', search_params, gotData);
 
 function gotData(err, data, response) {
- 	var tweets = data.statuses;
- 	for (var i = 0; i < tweets.length; i++) {
- 		console.log(tweets[i].text);
- 	}
+	for (var i = 0; i < data.statuses.length; i++) {
+		var tweet = data.statuses[i].text;
+ 		if(tweet.search("I\'m") != -1) {
+			var output = tweet.slice(tweet.search("I\'m") + 4, tweet.search(/\,|\.|\!|\?|$/));
+			if (output !== "") {
+				var to_post = "\n@" + data.statuses[i].user.screen_name + " Nice to meet you " + output + ", I\'m Dad!\n";
+				console.log(to_post);
+				
+				//  tweet the result
+				var post_params = { status: to_post };
+				T.post('statuses/update', post_params, postedTweet);
+
+				function postedTweet(err, data, response) {
+				  if (err) {
+				  	console.log(err);
+				  }
+				  console.log("Tweet posted");
+				}
+			}
+		}
+	}
 }
+
+//Process tweets as message
 
 //  tweet 'hello world!'
-var post_params = { status: 'hello world!' };
+// var post_params = { status: 'hello world!' };
 
-T.post('statuses/update', post_params, postedTweet);
+// T.post('statuses/update', post_params, postedTweet);
 
-function postedTweet(err, data, response) {
-  if (err) {
-  	console.log(err);
-  }
-  console.log("Tweet posted");
-}
+// function postedTweet(err, data, response) {
+//   if (err) {
+//   	console.log(err);
+//   }
+//   console.log("Tweet posted");
+// }
