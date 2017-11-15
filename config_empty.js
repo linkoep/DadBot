ba88e8f904c4969bfd3d66406bb490a0780324cd
Linus Koepfer <linkoep@gmail.com>
2017-11-15 01:41:19 -0500

    Set up API, created config file for Twitter authentiation, and gitignore to hid config file

---
+++ config_empty.js
@@ -0,0 +1,10 @@
+//This config file contains the authentication for the twitter API
+//For obvious reasons, I have not provided my keys here.
+//To make this app yourself, obtain the necessary keys and tokens, place them in this file, and save it as config.js
+
+module.exports = {
+	consumer_key:         '...',
+ 	consumer_secret:      '...',
+ 	access_token:         '...',
+ 	access_token_secret:  '...'
+}
