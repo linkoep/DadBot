78142e0a948fa3b2024b25bf2d7e815d8526eef0
Linus Koepfer <linkoep@gmail.com>
2017-11-15 13:09:15 -0500

    Added setinterval, bot tweets 100 times / hour

--- index.js
+++ index.js
@@ -5,42 +5,56 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-//  search twitter for all tweets containing the word 'I'm'
-var search_params = { 
-	q: 'I\'m -filter:retweets filter:safe -filter:yeu :)',
-	lang: 'en', 
-	count: 10 
-};
+DadTweet();
+setInterval(DadTweet, 37*1000);
 
-T.get('search/tweets', search_params, gotData);
+function DadTweet() {
+	//  search twitter for tweets containing the word 'I'm'
+	//Filters out retweets, unsafe content, 
+	//focuses on what twitter deems as "positive"
+	var search_params = { 
+		q: 'I\'m -filter:retweets filter:safe -filter:yeu :)',
+		lang: 'en', 
+		count: 10 
+	};
 
-function gotData(err, data, response) {
-	for (var i = 0; i < data.statuses.length; i++) {
-		if(true) {
-			var tweet = data.statuses[i].text;
-			console.log(tweet);
-	 		if(tweet.search(/\bI\'m\b|\bi\'m\b/) != -1) {
-				var output = tweet.slice(tweet.search(/\bI\'m\b|\bi\'m\b/) + 4, tweet.search(/\,|\.|\!|\?|$|htpps/));
-				if (output !== "") {
-					var to_post = "\n@" + data.statuses[i].user.screen_name + " Nice to meet you " + output + ", I\'m Dad!\n";
-					console.log(to_post);
-					
-					 //tweet the result
-					var post_params = { 
-						status: to_post,
-						in_reply_to_status_id: data.statuses[i].id_str
-					};
-					T.post('statuses/update', post_params, postedTweet);
+	T.get('search/tweets', search_params, gotData);
 
-					function postedTweet(err, data, response) {
-					  if (err) {
-					  	console.log(err);
-					  }
-					  console.log("Tweet posted");
+	//Handles search results
+	function gotData(err, data, response) {
+		//Iterate through results (will break after first tweet is posted)
+		for (var i = 0; i < data.statuses.length; i++) {
+			//Additional search refinment can be done here
+			if(true) {
+				var tweet = data.statuses[i].text;
+				//console.log(tweet); //Print for debugging
+		 		if(tweet.search(/\bI\'m\b|\bi\'m\b/) != -1) {
+					//Find relevant portion of tweet
+					var output = tweet.slice(tweet.search(/\bI\'m\b|\bi\'m\b/) + 4, tweet.search(/\,|\.|\!|\?|$|htpps|…|\:/));
+					if (output !== "") {
+						var to_post = "\n@" + data.statuses[i].user.screen_name + " Nice to meet you " + output + ", I\'m Dad!\n";
+						//console.log(to_post); //print for debugging
+						
+						//tweet the result
+						var post_params = { 
+							status: to_post,
+							in_reply_to_status_id: data.statuses[i].id_str
+						};
+						T.post('statuses/update', post_params, postedTweet);
+
+						//WHat to do after tweeting
+						function postedTweet(err, data, response) {
+						  if (err) {
+						  	console.log(err);
+						  }
+						  console.log("Tweet posted");
+						}
+						break;
 					}
 				}
 			}
 		}
 	}
 }
+
 
