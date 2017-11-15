dca88fe519a46718a8c4257a1d162b6a1e9f827c
Linus Koepfer <linkoep@gmail.com>
2017-11-15 12:34:43 -0500

    Refined search and tweet query Bot now uses both upercase and lowercase, avoids unsafe content, avoid retweets Bot replies to what twitter considers "positive attitudes" Instead of @mentioning, bot tweets are now actually replies

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var T = new Twit(config);
 
 //  search twitter for all tweets containing the word 'I'm'
 var search_params = { 
-	q: 'I\'m', 
+	q: 'I\'m -filter:retweets filter:safe -filter:yeu :)',
+	lang: 'en', 
 	count: 10 
 };
 
@@ -15,38 +16,31 @@ T.get('search/tweets', search_params, gotData);
 
 function gotData(err, data, response) {
 	for (var i = 0; i < data.statuses.length; i++) {
-		var tweet = data.statuses[i].text;
- 		if(tweet.search("I\'m") != -1) {
-			var output = tweet.slice(tweet.search("I\'m") + 4, tweet.search(/\,|\.|\!|\?|$/));
-			if (output !== "") {
-				var to_post = "\n@" + data.statuses[i].user.screen_name + " Nice to meet you " + output + ", I\'m Dad!\n";
-				console.log(to_post);
-				
-				//  tweet the result
-				var post_params = { status: to_post };
-				T.post('statuses/update', post_params, postedTweet);
+		if(true) {
+			var tweet = data.statuses[i].text;
+			console.log(tweet);
+	 		if(tweet.search(/\bI\'m\b|\bi\'m\b/) != -1) {
+				var output = tweet.slice(tweet.search(/\bI\'m\b|\bi\'m\b/) + 4, tweet.search(/\,|\.|\!|\?|$|htpps/));
+				if (output !== "") {
+					var to_post = "\n@" + data.statuses[i].user.screen_name + " Nice to meet you " + output + ", I\'m Dad!\n";
+					console.log(to_post);
+					
+					 //tweet the result
+					var post_params = { 
+						status: to_post,
+						in_reply_to_status_id: data.statuses[i].id_str
+					};
+					T.post('statuses/update', post_params, postedTweet);
 
-				function postedTweet(err, data, response) {
-				  if (err) {
-				  	console.log(err);
-				  }
-				  console.log("Tweet posted");
+					function postedTweet(err, data, response) {
+					  if (err) {
+					  	console.log(err);
+					  }
+					  console.log("Tweet posted");
+					}
 				}
 			}
 		}
 	}
 }
 
-//Process tweets as message
-
-//  tweet 'hello world!'
-// var post_params = { status: 'hello world!' };
-
-// T.post('statuses/update', post_params, postedTweet);
-
-// function postedTweet(err, data, response) {
-//   if (err) {
-//   	console.log(err);
-//   }
-//   console.log("Tweet posted");
-// }
